531e5f39011233d962e814888e69dcaf91307ad2
grishma2007 <grishmaajani@gmail.com>
2026-03-05 12:26:07 +0530

    update done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
 )
     .then(()=> {
       sessionStorage.setItem("isLoggedIn","true")
-      navigate("/info")
+      navigate("/Info")
       
     })
       .catch(function (error) {
