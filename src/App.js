486f458fa682496d6bf9f64b4d5b8d21cdb6d007
grishma2007 <grishmaajani@gmail.com>
2026-03-05 14:25:47 +0530

    update done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login.jsx';
 import Register from './pages/register.jsx';
-import Info from './pages/Info.jsx';
+
 import Products from './pages/Products.jsx';
 import Allpropducts from './pages/Allproducts.jsx';
 import EditProduct from './pages/EditProduct.jsx';
 import OrderDetails from './pages/Orders/OrderDetails.jsx';
 import OrderList from './pages/Orders/OrderList.jsx';
+import Info from './pages/Info.jsx';
 function App() {
   return (
     <div className="App">
@@ -16,7 +17,7 @@ function App() {
       <Route path="/" element={ <Register />}/>
       <Route path="/register" element={ <Register />}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/info" element={<info/>}/>
+      <Route path="/info" element={<Info/>}/>
       <Route path='/products/add' element={<Products/>}/>
       <Route path="/products/all" element={<Allpropducts/>} />
       <Route path="/products/edit/:id" element={<EditProduct />} />
